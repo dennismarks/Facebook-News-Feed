a9fa2d7358c842bf3a27d1f80c683cce3e579666
dennismarks <dennismarkshome@gmail.com>
2019-07-02 11:00:00 -0600

    add friends request header

--- Facebook News Feed/FriendRequestController.swift
+++ Facebook News Feed/FriendRequestController.swift
@@ -17,7 +17,10 @@ class FriendsRequestController: UITableViewController {
         super.viewDidLoad()
         
         self.navigationItem.title = "Friend Requests"
-
+        
+        tableView.sectionHeaderHeight = 26
+        
+        tableView.register(FriendRequestHeader.self, forHeaderFooterViewReuseIdentifier: headerId)
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -36,6 +39,49 @@ class FriendsRequestController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: headerId) as! FriendRequestHeader
+        return header
+    }
 
 }
 
+class FriendRequestHeader: UITableViewHeaderFooterView {
+    
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "FRIEND REQUESTS"
+        label.font = UIFont.systemFont(ofSize: 10)
+        label.textColor = UIColor(white: 0.4, alpha: 1)
+        return label
+    }()
+    
+    let bottomBorderWidth: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.rgb(229, green: 231, blue: 235)
+        return view
+    }()
+    
+    func setupViews() {
+        addSubview(nameLabel)
+        addSubview(bottomBorderWidth)
+        
+        addConstraintsWithFormat("H:|-8-[v0]-8-|", views: nameLabel)
+        addConstraintsWithFormat("H:|[v0]|", views: bottomBorderWidth)
+        
+        addConstraintsWithFormat("V:|[v0][v1(0.5)]|", views: nameLabel, bottomBorderWidth)
+        
+    }
+    
+}
